f906428fd787e622df65c82f9cafb793a9487eb5
AlanD20 <aland20@pm.me>
2023-08-19 21:03:19 +0200

    Add user routes

--- app/api/user.go
+++ app/api/user.go
@@ -1,7 +1,8 @@
 package api
 
 import (
-	"github.com/aland20/go-noting/app/model"
+	"strings"
+
 	"github.com/aland20/go-noting/app/response"
 	"github.com/labstack/echo/v4"
 )
@@ -12,20 +13,23 @@ type UserController struct {
 
 func BindUserApi(app *BaseApp, rg *echo.Group) {
 
-	api := &UserController{App: app}
+	ctrl := &UserController{App: app}
 
-	rg.GET("/users", api.IndexUser).Name = "user.index"
-	rg.GET("/users/:username", api.ShowUser).Name = "user.show"
-	rg.POST("/users", api.StoreUser).Name = "user.store"
-	rg.PATCH("/users/:username", api.UpdateUser).Name = "user.update"
-	rg.DELETE("/users/:username", api.DestroyUser).Name = "user.destroy"
+	rg.GET("/users", ctrl.IndexUser).Name = "user.index"
+	rg.GET("/users/:username", ctrl.ShowUser).Name = "user.show"
+	rg.POST("/users", ctrl.StoreUser).Name = "user.store"
+	rg.PATCH("/users/:username", ctrl.UpdateUser).Name = "user.update"
+	rg.DELETE("/users/:username", ctrl.DestroyUser).Name = "user.destroy"
 }
 
-func (ctr *UserController) IndexUser(ctx echo.Context) error {
+func (ctrl *UserController) IndexUser(ctx echo.Context) error {
 
-	users := &[]model.User{}
+	users := &[]User{}
 
-	err := ctr.App.Connection.Select("id", "username", "email", "created_at", "updated_at").
+	err := ctrl.App.Connection.
+		Model(&User{}).
+		Select("id", "username", "email", "created_at", "updated_at").
+		Where("deleted_at IS NULL").
 		Find(&users).
 		Error
 
@@ -36,12 +40,12 @@ func (ctr *UserController) IndexUser(ctx echo.Context) error {
 	return response.Ok(ctx, users)
 }
 
-func (ctr *UserController) ShowUser(ctx echo.Context) error {
+func (ctrl *UserController) ShowUser(ctx echo.Context) error {
 
-	user := &model.User{}
+	user := &User{}
 	username := ctx.Param("username")
 
-	err := ctr.App.Connection.Where("username = ?", username).Take(&user).Error
+	err := ctrl.App.Connection.Where("username = ?", username).Take(&user).Error
 
 	if err != nil {
 		return response.NotFound(ctx)
@@ -50,29 +54,38 @@ func (ctr *UserController) ShowUser(ctx echo.Context) error {
 	return response.Ok(ctx, user)
 }
 
-func (ctr *UserController) StoreUser(ctx echo.Context) error {
+func (ctrl *UserController) StoreUser(ctx echo.Context) error {
 
-	u := &model.UserSchema{}
+	u := &UserSchema{}
 
 	// Bind incoming form to u (UserSchema) type
 	if err := ctx.Bind(&u); err != nil {
 		return response.BadRequest(ctx)
 	}
 
-	// Construct User type from UserSchema
-	user := u.NewUser()
+	// Create user
+	user := &User{
+		UserSchema: UserSchema{
+			Username: strings.ToLower(u.Username),
+			Email:    strings.ToLower(u.Email),
+			Password: u.Password,
+		},
+	}
 
-	if err := ctr.App.Connection.Create(&user).Error; err != nil {
+	// Generate UUID
+	user.GenerateId()
+
+	if err := ctrl.App.Connection.Create(&user).Error; err != nil {
 		return response.BadRequest(ctx)
 	}
 
 	return response.Created(ctx, &u)
 }
 
-func (ctr *UserController) UpdateUser(ctx echo.Context) error {
+func (ctrl *UserController) UpdateUser(ctx echo.Context) error {
 
-	user := &model.User{}
-	u := &model.UserSchema{}
+	u := &UserSchema{}
+	user := &User{}
 	username := ctx.Param("username")
 
 	// Bind incoming form to u (UserSchema) type
@@ -81,36 +94,46 @@ func (ctr *UserController) UpdateUser(ctx echo.Context) error {
 	}
 
 	// Retrieve user
-	err := ctr.App.Connection.Where("username = ?", username).Take(&user).Error
+	err := ctrl.App.Connection.
+		Model(&User{}).
+		Select("id", "username", "email", "created_at", "updated_at").
+		Where("username = ? AND deleted_at IS NULL", username).
+		Take(&user).
+		Error
 
 	if err != nil {
 		return response.NotFound(ctx)
 	}
 
-	// Update user (User) retrieved from DB values with u (User Schema)
-	u.UpdateUser(user)
+	// Update user
+	user.Username = strings.ToLower(u.Username)
+	user.Email = strings.ToLower(u.Email)
+	user.Password = u.Password
 
 	// Run update query to database
-	if err := ctr.App.Connection.Updates(&user).Error; err != nil {
+	if err := ctrl.App.Connection.Updates(&user).Error; err != nil {
 		return response.UnprocessableEntity(ctx)
 	}
+
+	// Unset sensitive fields
+	user.Password = ""
 
 	return response.Update(ctx, user)
 }
 
-func (ctr *UserController) DestroyUser(ctx echo.Context) error {
+func (ctrl *UserController) DestroyUser(ctx echo.Context) error {
 
-	user := &model.User{}
+	user := &User{}
 	username := ctx.Param("username")
 
-	errTake := ctr.App.Connection.Where("username = ?", username).Take(&user).Error
+	errTake := ctrl.App.Connection.Where("username = ?", username).Take(&user).Error
 
 	if errTake != nil {
 		return response.NotFound(ctx)
 	}
 
 	// Delete user for given username
-	errDel := ctr.App.Connection.Where("username = ?", username).Delete(&user).Error
+	errDel := ctrl.App.Connection.Where("username = ?", username).Delete(&user).Error
 
 	if errDel != nil {
 		return response.UnprocessableEntity(ctx)
